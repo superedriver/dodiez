b6d218e3b3dda43dd4d962cc99586d1e4b720075
superedriver <superedriver@gmail.com>
2016-07-13 15:39:53 +0300

    added more tests

--- spec/dodiez_spec.rb
+++ spec/dodiez_spec.rb
@@ -7,11 +7,15 @@ describe Dodiez do
   end
 
   describe "without similar Symbol and String keys" do
-    let(:hash) { Dodiez::CoolHash.new({ qaz: 1, "qwert" => 2, 3 => 4, [1] => 5, {e: 0} => 6}) }
+    let(:hash) { CoolHash.new({ qaz: 1, "qwert" => 2, 3 => 4, [1] => 5, {e: 0} => 6}) }
 
     context "symbol key" do
       it "can access as symbol" do
         expect(hash[:qaz]).to eq(1)
+      end
+
+      it "as string should not be nil" do
+        expect(hash["qaz"]).not_to be_nil
       end
 
       it "can access as string" do
@@ -20,6 +24,10 @@ describe Dodiez do
     end
 
     context "string key" do
+      it "as symbol should not be nil" do
+        expect(hash[:qwert]).not_to be_nil
+      end
+
       it "can access as symbol" do
         expect(hash[:qwert]).to eq(2)
       end
@@ -46,10 +54,36 @@ describe Dodiez do
         expect(hash[{e: 0}]).to eq(6)
       end
     end
+
+    describe "unexpected key values" do
+      it "symbol" do
+        expect(hash[:qaz1]).to be_nil
+      end
+
+      it "string" do
+        expect(hash["qaz1"]).to be_nil
+      end
+
+      it "number" do
+        expect(hash[7]).to be_nil
+      end
+
+      it "nil" do
+        expect(hash[nil]).to be_nil
+      end
+
+      it "true" do
+        expect(hash[true]).to be_nil
+      end
+
+      it "false" do
+        expect(hash[false]).to be_nil
+      end
+    end
   end
 
   describe "similar Symbol and String keys have their own values" do
-    let(:hash) { Dodiez::CoolHash.new({ qwerty: 1, "qwerty" => 2 }) }
+    let(:hash) { CoolHash.new({ qwerty: 1, "qwerty" => 2 }) }
 
     it "symbol key" do
       expect(hash[:qwerty]).to eq(1)
