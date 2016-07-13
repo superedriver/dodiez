3f6c59d8a0bbec65505c2940bf492040dfd3f9ae
superedriver <superedriver@gmail.com>
2016-07-13 13:22:15 +0300

    changed namespace

--- lib/dodiez.rb
+++ lib/dodiez.rb
@@ -1,22 +1,20 @@
 require "dodiez/version"
 
-module Dodiez
-  class CoolHash
-    def initialize(hash)
-      @hash = hash
-    end
-
-    def [](key)
-      if needed_type?(key)
-        @hash[key] ? @hash[key] : @hash[key.to_sym] || @hash[key.to_s]
-      else
-        @hash[key]
-      end
-    end
-
-    private
-      def needed_type?(key)
-        key.class == String || key.class == Symbol
-      end
+class CoolHash
+  def initialize(hash)
+    @hash = hash
   end
+
+  def [](key)
+    if needed_type?(key)
+      @hash[key] ? @hash[key] : @hash[key.to_sym] || @hash[key.to_s]
+    else
+      @hash[key]
+    end
+  end
+
+  private
+    def needed_type?(key)
+      key.class == String || key.class == Symbol
+    end
 end
