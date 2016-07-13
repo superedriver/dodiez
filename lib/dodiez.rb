1a7e6b3e04d6d0bb1ae5ee683242f90099153478
superedriver <superedriver@gmail.com>
2016-07-13 11:49:27 +0300

    added tests

--- lib/dodiez.rb
+++ lib/dodiez.rb
@@ -1,44 +1,22 @@
 require "dodiez/version"
 
-class << Hash
-  def [](key)
-    self[converted_key(key)]
-  end
-
-  def []=(key, value)
-    self[converted_key(key)] = value
-  end
-
-  def converted_key(key)
-    if key.class == String
-      key.to_sym
-    else
-      key
-    end
-  end
-end
-
-class Dodiez < Hash
-  class << self
-    def to_cool_hash(hash)
-      return "You can modify only hashes" unless hash.class == Hash
-      new_hash = {}
-
-      hash.each do |key, value|
-        new_hash[converted_key(key)] = value
-      end
-      new_hash
+module Dodiez
+  class CoolHash
+    def initialize(hash)
+      @hash = hash
     end
 
-    def converted_key(key)
-      if key.class == String
-        key.to_sym
+    def [](key)
+      if needed_type?(key)
+        @hash[key] ? @hash[key] : @hash[key.to_sym] || @hash[key.to_s]
       else
-        key
+        @hash[key]
       end
     end
+
+    private
+      def needed_type?(key)
+        key.class == String || key.class == Symbol
+      end
   end
 end
-
-#  h = { "qw" => 1 }
-#  h1 = Dodiez.to_cool_hash(h)
--- spec/dodiez_spec.rb
+++ spec/dodiez_spec.rb
@@ -1,11 +1,62 @@
 require 'spec_helper'
 
 describe Dodiez do
-  it 'has a version number' do
+
+  it "has a version number" do
     expect(Dodiez::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(1).to eq(1)
+  describe "without similar Symbol and String keys" do
+    let(:hash) { Dodiez::CoolHash.new({ qaz: 1, "qwert" => 2, 3 => 4, [1] => 5, {e: 0} => 6}) }
+
+    context "symbol key" do
+      it "can access as symbol" do
+        expect(hash[:qaz]).to eq(1)
+      end
+
+      it "can access as string" do
+        expect(hash["qaz"]).to eq(1)
+      end
+    end
+
+    context "string key" do
+      it "can access as symbol" do
+        expect(hash[:qwert]).to eq(2)
+      end
+
+      it "can access as string" do
+        expect(hash["qwert"]).to eq(2)
+      end
+    end
+
+    context "fixnum key" do
+      it "can access as fixnum" do
+        expect(hash[3]).to eq(4)
+      end
+    end
+
+    context "array key" do
+      it "can access as array" do
+        expect(hash[[1]]).to eq(5)
+      end
+    end
+
+    context "hash key" do
+      it "can access as hash" do
+        expect(hash[{e: 0}]).to eq(6)
+      end
+    end
+  end
+
+  describe "similar Symbol and String keys have their own values" do
+    let(:hash) { Dodiez::CoolHash.new({ qwerty: 1, "qwerty" => 2 }) }
+
+    it "symbol key" do
+      expect(hash[:qwerty]).to eq(1)
+    end
+
+    it "string key" do
+      expect(hash["qwerty"]).to eq(2)
+    end
   end
 end
